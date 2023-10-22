f105de35d8760380a25cd1b551c75a38a1282c75
Lynda Chiwetelu <lyndachiwe@gmail.com>
2023-10-22 22:52:38 +0200

    commit starter code for wideget tutorial

---
+++ WaterDrops/LogManager.swift
@@ -0,0 +1,42 @@
+//
+
+import Foundation
+import CoreData
+
+
+class LogManager {
+    static var shared = LogManager()
+    
+    func getLatestLog() -> Log? {
+        let viewContext = PersistenceController.shared.container.viewContext
+        let fetchRequest: NSFetchRequest<Log> = Log.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchRequest.fetchLimit = 1
+        
+        do {
+            let latestLogs = try viewContext.fetch(fetchRequest)
+            guard latestLogs.first != nil && Calendar.current.isDateInToday(latestLogs.first!.timestamp!) else { return nil }
+            return latestLogs.first
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+
+    
+    func saveLog(waterAmount: Int) -> Void {
+        let viewContext = PersistenceController.shared.container.viewContext
+        let log = Log(context: viewContext)
+        log.amount = Int16(waterAmount)
+        log.timestamp = Date.now
+        
+        do {
+            try viewContext.save()
+        } catch {
+            print(error)
+        }
+    }
+
+}
